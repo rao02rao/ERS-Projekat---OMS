032bb0dd466e263a91ae795da757cf1dcb0668f6
rao02rao <92275496+rao02rao@users.noreply.github.com>
2024-01-05 23:17:09 +0100

    Applying SOLID principles to ElectricalElement.cs

---
+++ OutageManagementSystem/IElectricalElement.cs
@@ -0,0 +1,14 @@
+﻿namespace OutageManagementSystem
+{
+    // Interfejs koji definiše osnovne karakteristike električnog elementa.
+    public interface IElectricalElement
+    {
+        int ElementId { get; }
+        string Name { get; }
+        string Type { get; }
+        double Latitude { get; }
+        double Longitude { get; }
+        string VoltageLevel { get; }
+        string ToXml();
+    }
+}

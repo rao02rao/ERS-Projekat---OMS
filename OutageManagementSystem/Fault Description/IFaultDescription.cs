ea581b7e7c71b07cc677eaf0abc3f30fde9b5097
rao02rao <92275496+rao02rao@users.noreply.github.com>
2024-01-05 23:42:31 +0100

    Applying SOLID principles to FaultDescription.cs

---
+++ OutageManagementSystem/Fault Description/IFaultDescription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OutageManagementSystem
+{
+    // Interfejs koji definiše osnovne karakteristike opisa kvara.
+    public interface IFaultDescription
+    {
+        string FaultId { get; }
+        DateTime TimeOfCreation { get; }
+        string Status { get; }
+        string ShortDescription { get; }
+        string Description { get; }
+        int ElementId { get; }
+        List<Action> Actions { get; }
+        string ToXml();
+    }
+}

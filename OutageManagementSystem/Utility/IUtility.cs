15108a80432735271e7daf20bf122d99cfface66
rao02rao <92275496+rao02rao@users.noreply.github.com>
2024-01-06 14:31:05 +0100

    Applying SOLID principles to UtilityClass.cs

---
+++ OutageManagementSystem/Utility/IUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutageManagementSystem
+{
+    public interface IUtility
+    {
+        double PromptForDouble(string message); // Metoda za traženje unosa decimalnog broja od korisnika
+    }
+
+}
